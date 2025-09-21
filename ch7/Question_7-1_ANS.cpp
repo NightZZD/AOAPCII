b4ce258d618aa75a3a2759888a2f1c9e130f5ef6
nightzzd <zzd@mail.nankai.edu.cn>
2025-09-21 14:34:01 +0800

    fix bug- ans init wrong

--- ch7/Question_7-1_ANS.cpp
+++ ch7/Question_7-1_ANS.cpp
@@ -1,7 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-    
+int main(){
+    int n;
+    cin >> n;
+    int caseNum = 0;
+    vector<pair<int,int>> ans;
+    auto check = [](int a,int b){
+        int cnt[10]={};
+        //5位数，取5次
+        for(int i=0;i<5;i++)
+        {
+            cnt[a%10]++;
+            a/=10;
+            cnt[b%10]++;
+            b/=10;
+        }
+        //检查是否：用且只用了一次
+        for(int i=0;i<10;i++)
+            if(cnt[i]!=1) return false;
+        return true;
+    };
+    while (cin>>n)
+    {
+        if (caseNum++) cout<<'\n';
+        for(int fjhik=01234;;fjhik++)
+        {
+            int abcde = n*fjhik;
+            if (abcde>98765) break;
+            if check(abcde,fjhik)
+                ans.push_back({abcde,fjhik});
+        }
+        if (ans.size()==0)
+            cout<<"There are no solutions for "<<n<<".\n";
+        else
+            for(auto p:ans)
+                printf("%05d / %05d = %d\n",p.first,p.second,n);
+    }
     return 0;
 }
